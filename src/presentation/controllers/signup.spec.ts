d639f279bfb8f778b05a75e64ca9f32bacb440d4
Yuri Fernandes da Silva <ti2@gimi.com.br>
2022-02-24 14:27:02 -0300

    test: ensure SignUpController returns 400 if no name is provided

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,17 @@
+import { SignUpController } from './SignUp'
+
+describe('SignUp Controller', () => {
+  test('Should return 400 if no name is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: 'email@example.com',
+        password: 'password',
+        passwordConfirmation: 'password'
+
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
