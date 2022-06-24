da1b7455bc7c3b37327237fea4d59003e8168d5d
Yudota <yuri.fernandes.961@gmail.com>
2022-06-24 17:25:00 -0300

    feat: add LogMongoRepository to SignUpFactory

--- src/main/routes/signup.routers.test.ts
+++ src/main/routes/signup.routers.test.ts
@@ -1,5 +1,5 @@
-// import request from 'supertest'
-// import app from '../config/app'
+import request from 'supertest'
+import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 
 describe('Signup Routes', () => {
@@ -16,16 +16,15 @@ describe('Signup Routes', () => {
     await accountCollection.deleteMany({})
   })
 
-  test('should first', () => { expect(1).toBe(1) })
-  // test('Should return an account on success', async () => {
-  //   const response = await request(app)
-  //     .post('/api/signup')
-  //     .send({
-  //       name: 'Yuri',
-  //       email: 'yuri.fernandes.961@gmail.com',
-  //       password: 'any_password',
-  //       passwordConfirmation: 'any_password'
-  //     })
-  //   expect(response.status).toBe(201)
-  // })
+  test('Should return an account on success', async () => {
+    const response = await request(app)
+      .post('/api/signup')
+      .send({
+        name: 'Yuri',
+        email: 'yuri.fernandes.961@gmail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      })
+    expect(response.status).toBe(201)
+  })
 })
