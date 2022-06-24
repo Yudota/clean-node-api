527b430dfc38a5d15e83bec11a040c1db7c7f06b
Yudota <yuri.fernandes.961@gmail.com>
2022-06-24 09:57:32 -0300

    feat: new http-helper ok

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -9,10 +9,10 @@ export const serverError = (error: Error): HttpResponse => ({
   statusCode: 500,
   body: new ServerError(error.stack)
 })
-// export const ok = (data: any): HttpResponse => ({
-//   statusCode: 200,
-//   body: data
-// })
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data
+})
 export const created = (data: any): HttpResponse => ({
   statusCode: 201,
   body: data
