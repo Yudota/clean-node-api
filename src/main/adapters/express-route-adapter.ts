31435f290c2351e86c80cbd5337f0b5871adb3d9
Yudota <yuri.fernandes.961@gmail.com>
2022-06-27 16:14:06 -0300

    refactor: change status comparation to a 201 created

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -6,10 +6,10 @@ export const adaptRoute = (controller: Controller): any => {
       body: req.body
     }
     const httpResponse = await controller.handle(httpRequest)
-    if (httpResponse.statusCode === 200) {
-      res.status(httpResponse.statusCode).json(httpResponse.body)
+    if (httpResponse.statusCode === 201) {
+      return res.status(httpResponse.statusCode).json(httpResponse.body)
     } else {
-      res.status(httpResponse.statusCode).json({
+      return res.status(httpResponse.statusCode).json({
         error: httpResponse.body.message
       })
     }
