bfd2eabcd6e639436e91a39fd95bfc5bd53dc34f
Yudota <yuri.fernandes.961@gmail.com>
2022-08-15 14:02:53 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
-  port: process.env.PORT || 5050
+  port: process.env.PORT || 5050,
+  jwtSecret: process.env.JWT_SECRET || 'cb3807a07e6c07c9c8d7412f461176b4',
 }
--- src/presentation/controllers/login/login-controller.ts
+++ src/presentation/controllers/login/login-controller.ts
@@ -2,8 +2,8 @@ import { Controller, HttpRequest, HttpResponse, Authentication, Validation } fro
 import { badRequest, ok, serverError, unauthorizedError } from '../../helpers/http/http-helper'
 export class LoginController implements Controller {
   constructor (
+    private readonly authentication: Authentication,
     private readonly validation: Validation,
-    private readonly authentication: Authentication
   ) { }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
