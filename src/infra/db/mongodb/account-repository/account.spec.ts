8c17f69ede8809f3515d629166413be4915ab9bd
Yudota <yuri.fernandes.961@gmail.com>
2022-08-15 00:26:37 -0300

    test: ensure AccountMongoRepository returns an account on loadByEmail success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,6 +1,9 @@
 import { MongoHelper } from '../helpers/mongo-helper'
 import { AccountMongoRepository } from './account'
+import { Collection } from 'mongodb'
+import { AccountModel } from '../../../../data/usecases/authentication/db-authentication-protocols'
 
+let accountCollection: Collection
 describe('Account Mongo Repository', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
@@ -11,7 +14,7 @@ describe('Account Mongo Repository', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -19,17 +22,42 @@ describe('Account Mongo Repository', () => {
     return new AccountMongoRepository()
   }
 
-  test('Should return an account on success', async () => {
+  const isValidAccount = (account: AccountModel, method: string): void => {
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email@mail.com')
+    expect(account.password).toBe('any_password')
+    console.log(`${method} - is a validAccount\n`)
+  }
+
+  test('Should return an account on add success', async () => {
     const sut = makeSut()
     const account = await sut.add({
       name: 'any_name',
       email: 'any_email@mail.com',
       password: 'any_password'
     })
-    expect(account).toBeTruthy()
-    expect(account.id).toBeTruthy()
-    expect(account.name).toBe('any_name')
-    expect(account.email).toBe('any_email@mail.com')
-    expect(account.password).toBe('any_password')
+    isValidAccount(account, 'Should return an account on add success')
+    // expect(account).toBeTruthy()
+    // expect(account.id).toBeTruthy()
+    // expect(account.name).toBe('any_name')
+    // expect(account.email).toBe('any_email@mail.com')
+    // expect(account.password).toBe('any_password')
+  })
+  test('Should return an account on loadByEmail success', async () => {
+    const sut = makeSut()
+    await accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    const account = await sut.loadByEmail('any_email@mail.com')
+    isValidAccount(account, 'Should return an account on loadByEmail success')
+    // expect(account).toBeTruthy()
+    // expect(account.id).toBeTruthy()
+    // expect(account.name).toBe('any_name')
+    // expect(account.email).toBe('any_email@mail.com')
+    // expect(account.password).toBe('any_password')
   })
 })
