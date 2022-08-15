50641e7818bcf7024dea9e274102623d6eb58f7d
Yudota <yuri.fernandes.961@gmail.com>
2022-08-15 13:32:48 -0300

    refactor: remove duplicated code

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,12 +1,8 @@
 import { AccountModel, AddAccount, AddAccountModel, Hasher } from './db-add-account-protocols'
 export class DbAddAccount implements AddAccount {
-  private readonly hasher: Hasher
-  private readonly addAccountRepository: AddAccount
-
-  constructor (hasher: Hasher, addAccountRepository: AddAccount) {
-    this.hasher = hasher
-    this.addAccountRepository = addAccountRepository
-  }
+  constructor (
+    private readonly hasher: Hasher,
+    private readonly addAccountRepository: AddAccount) { }
 
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const hashedPassord = await this.hasher.encrypt(accountData.password)
--- src/infra/cryptography/bcrypt-adapter/bcrypt-adapter.ts
+++ src/infra/cryptography/bcrypt-adapter/bcrypt-adapter.ts
@@ -1,10 +1,7 @@
 import { Hasher } from '../../../data/protocols/criptography/hasher'
 import bcrypt from 'bcrypt'
 export class BcryptAdapter implements Hasher {
-  private readonly salt: number
-  constructor (salt: number) {
-    this.salt = salt
-  }
+  constructor (private readonly salt: number) { }
 
   async encrypt (value: string): Promise<string> {
     return await bcrypt.hash(value, this.salt)
