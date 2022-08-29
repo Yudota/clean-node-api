70f4b82d13a779608ce102dbe2943c0607f61810
Yudota <yuri.fernandes.961@gmail.com>
2022-08-28 23:02:18 -0300

    chore: update docker-compose

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/clean-node-api',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || 'cb3807a07e6c07c9c8d7412f461176b4',
 }
